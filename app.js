350899806977051d077ecb6d19873e70653799d9
SUMIT KUMAR <sk69@iitbbs.ac.in>
2020-09-15 16:55:35 +0530

    added mongodb

--- app.js
+++ app.js
@@ -6,7 +6,8 @@ const ejs = require("ejs");
 const loDash = require("lodash");
 const mongoose = require('mongoose');
 
-mongoose.connect("mongodb://localhost:27017/blogDB", {useNewUrlParser: true, useUnifiedTopology: true});
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/blogDB", {useNewUrlParser: true, useUnifiedTopology: true});
 
 const homeStartingContent = "Hello World! Felling confused/frustrated/bored, come here and write whatever you want and let the world know what you are feeling.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
